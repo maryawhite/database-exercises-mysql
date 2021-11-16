2fbe2b8cf2a89d06d8c5cb4d5be2601370342431
Mary White <mary.a.white28@gmail.com>
2021-11-16 17:34:26 -0500

    completed exercise by running the appropriate commands in the terminal

--- albums_migration.sql
+++ albums_migration.sql
@@ -9,3 +9,11 @@ CREATE TABLE IF NOT EXISTS albums (
                                       genre TEXT NOT NULL,
                                       PRIMARY KEY (id)
 );
+
+# open a terminal, and run the script as codeup_test_user with the following command:
+#
+#
+# mysql -u codeup_test_user -p < albums_migration.sql
+# After running the script, connect to the MySQL server as you have done previously.
+#
+# USE the codeup_test_db and use DESCRIBE and SHOW CREATE to verify that your albums table has been successfully created.
