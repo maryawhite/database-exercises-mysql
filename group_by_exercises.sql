5e92435a90c64c3cac07f6c8c12e612fb53b03ee
Mary White <mary.a.white28@gmail.com>
2021-11-19 15:57:06 -0500

    added order by to the count

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -7,6 +7,8 @@ SELECT DISTINCT title FROM titles;
 # Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
 SELECT DISTINCT last_name from employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY last_name ASC;
 SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name ORDER BY last_name ASC;
+#the following would also work to find last names that start and end with e:
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name ORDER BY last_name;
 
 #Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 SELECT last_name, first_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name, first_name ORDER BY last_name ASC;
@@ -15,12 +17,9 @@ SELECT last_name, first_name FROM employees WHERE last_name LIKE 'E%' AND last_n
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 #Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
-SELECT COUNT(*), last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+SELECT COUNT(*) AS 'NUMBER OF PEOPLE', last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY COUNT(*);
 
 #Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names
-SELECT * FROM employees WHERE first_name = 'Irena'
-                           OR first_name = 'Vidya'
-                           OR first_name = 'Maya';
 SELECT COUNT(gender), gender FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' GROUP BY gender;
 
 # find all employees who had their 30th anniversary in the last 30 days, or are about to have their 30th anniversary in the next 30 days.
