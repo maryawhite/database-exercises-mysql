80f7b5a366d642065ba4a5a56f656d006ff15366
Mary White <mary.a.white28@gmail.com>
2021-11-19 12:54:23 -0500

    completed functions exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -11,7 +11,7 @@ SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND (month(b
 
 # Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini
 # dob ASC and hire date ASC
-SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY hire_date DESC, birth_date ASC;
+SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
 
 
 # For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
---
+++ group_by_exercises.sql
@@ -0,0 +1,31 @@
+USE employees;
+
+#In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
+SELECT DISTINCT title FROM titles;
+
+#Find your query for employees whose last names start and end with 'E'.
+# Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
+SELECT DISTINCT last_name from employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY last_name ASC;
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name ORDER BY last_name ASC;
+
+#Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT last_name, first_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name, first_name ORDER BY last_name ASC;
+
+#Find the unique last names with a 'q' but not 'qu'.
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+#Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+
+
+
+SELECT DISTINCT first_name
+FROM employees;
+# same as
+SELECT first_name
+FROM employees
+GROUP BY first_name;
+# columns that appear in select should also appear in Group by
+#multiple columns
+SELECT last_name, first_name
+FROM employees
+GROUP BY last_name, first_name;
