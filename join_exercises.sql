4ac61fa9057d5be2ea7bd39ca674cc9f5d8bf578
Mary White <mary.a.white28@gmail.com>
2021-11-22 23:10:16 -0500

    working on bonus, no go

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,16 +1,5 @@
 USE employees;
 
-SELECT * FROM employees LIMIT 10;
-SELECT * FROM departments LIMIT 10;
-
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
-FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
-
 #write a query that shows each department along with the name of the current manager for that department.
 #SELECT * FROM departments; this shows all 9 departments
 #SELECT * FROM dept_manager WHERE to_date = '9999-01-01'
@@ -58,15 +47,20 @@ ORDER BY dept_name;
 
 ##
 #Bonus Find the names of all current employees, their department name, and their current manager's name .
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(e.first_name, ' ', e.last_name) AS 'Manager'
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees as e
-    JOIN dept_manager AS dm
-         ON dm.emp_no = e.emp_no
-    JOIN departments d
-         ON dm.dept_no = d.dept_no
-    JOIN dept_emp AS de
-         ON e.emp_no = de.emp_no
-WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
-ORDER BY dept_name
-LIMIT 15;
+         JOIN dept_manager AS dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments d
+              ON dm.dept_no = d.dept_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY dept_name;
+
+#NOTES
+USE employees;
+SELECT * FROM employees LIMIT 10;
+SELECT * FROM departments LIMIT 10;
+SELECT * FROM dept_emp WHERE to_date = '9999-01-01' LIMIT 10;
+SELECT * FROM dept_manager WHERE to_date = '9999-01-01' LIMIT 10;
+
 
